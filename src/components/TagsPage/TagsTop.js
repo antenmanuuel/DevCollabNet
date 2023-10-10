5b775209cc100d9e75ad011770a86ecaa8f3847b
Anten Manuuel <anten.manuuel@stonybrook.edu>
2023-10-10 19:17:54 -0400

    implement logic for getting number of tags and questions

--- src/components/TagsPage/TagsTop.js
+++ src/components/TagsPage/TagsTop.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "../../stylesheets/TagsTop.css"
 import AskQuestionButton from '../QuestionsPage/AskQuestionButton'
+
+import Model from '../../models/model'
+
+
 const TagsTop = () => {
+  const model = new Model();
+  const [numOfTags, setNumOfTags] = useState(0);
+
+  useEffect(() => {
+    const tags = model.getAllTags();
+    setNumOfTags(tags.length);
+  })
   return (
     <div className='tagsTop'>
-      <h1 id='numTags'># Tags</h1>
-      <h2 id='allTags'>All Tags</h2>
+      <h1 id='numTags'>{numOfTags} Tags</h1>
+      <h2 id='allTags'> All Tags</h2>
       <AskQuestionButton />
     </div>
   )
