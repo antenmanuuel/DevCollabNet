e71884f8b4d5327024e1d4b21f44c4af75e5eb7a
Anten Manuuel <anten.manuuel@stonybrook.edu>
2023-10-10 20:14:06 -0400

    implement dynamically displaying the questions from model.js

--- src/components/QuestionsPage/QuestionTable.js
+++ src/components/QuestionsPage/QuestionTable.js
@@ -1,11 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "../../stylesheets/QuestionTable.css";
+import Model from "../../models/model";
+import formatDate from "../../utils/formatDate";
 
 const QuestionTable = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [questionsData, setQuestionData] = useState([]);
+  const model = new Model();
 
-export default QuestionTable
+  useEffect(() => {
+    const questions = model.getAllQuestions();
+    const formattedQuestions = questions.map((question) => {
+      const tagNames = question.tagIds.map((tagId) =>
+        model.getTagNameById(tagId)
+      );
+      const formattedDate = formatDate(new Date(question.askDate));
+
+      return {
+        ...question,
+        tagNames,
+        formattedDate,
+      };
+    });
+    setQuestionData(formattedQuestions);
+  }, []);
+
+  return (
+    <div className="questionTableContainer">
+      <div className="question">
+        <table id="question-table">
+          {questionsData.map((question, index) => (
+            <tr key={index} data-qid={question.qid} id="tr1">
+              <td id="td1">
+                <ul>
+                  <li id="num_ans">{question.ansIds.length} answers</li>
+                  <li id="num_ques">{question.views} views</li>
+                </ul>
+              </td>
+              <td id="td2">
+                <ul id="ulrow2">
+                  <li id="title_questions">
+                    <button id="link_ans">
+                      <p id="paraf">{question.title}</p>
+                    </button>
+                  </li>
+                  <li>
+                    <ul id="ulrow3">
+                      {question.tagNames.map((tagName, index) => (
+                        <li key={index} id={`keyword-${index + 1}`}>
+                          {tagName}
+                        </li>
+                      ))}
+                    </ul>
+                  </li>
+                </ul>
+              </td>
+              <td id="who_asked">
+                <p>
+                  <span style={{ color: "red" }}>{question.askedBy}</span>
+                  <span style={{ color: "gray" }}>
+                    {" "}
+                    asked {question.formattedDate}
+                  </span>
+                </p>
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default QuestionTable;
--- src/components/QuestionsPage/QuestionsContainer.js
+++ src/components/QuestionsPage/QuestionsContainer.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import "../../stylesheets/QuestionContainer.css"
 import QuestionTop from './QuestionTop'
+import QuestionTable from './QuestionTable'
 
 const QuestionsContainer = () => {
   return (
     <div className='questionContainer'>
       <QuestionTop />
+      <QuestionTable />
     </div>
   )
 }
