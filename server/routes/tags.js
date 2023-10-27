147ea405f36703daf98586b2384499f98e9dde35
Anten Manuuel <anten.manuuel@stonybrook.edu>
2023-10-27 08:18:24 -0400

    implemented backend routes that is to be used for the frontend

--- server/routes/tags.js
+++ server/routes/tags.js
@@ -4,5 +4,51 @@ const router = express.Router();
 const Questions = require('../models/questions');
 const Tags = require('../models/tags');
 
+// Error handling middleware
+const handleError = (err, res) => {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+};
+
+// Route to fetch all tags
+router.get('/', async (req, res) => {
+    try {
+        const tags = await Tags.find().exec();
+        res.send(tags);
+    } catch (err) {
+        handleError(err, res);
+    }
+});
+
+// Route to fetch a specific tag by its name
+router.get('/:tag', async (req, res) => {
+    try {
+        const tag = await Tags.find({ name: req.params.tag }).exec();
+        res.send(tag);
+    } catch (err) {
+        handleError(err, res);
+    }
+});
+
+// Route to fetch a specific tag by its ID
+router.get('/tag_id/:tag_id', async (req, res) => {
+    try {
+        const tag = await Tags.findById(req.params.tag_id).exec();
+        res.send(tag);
+    } catch (err) {
+        handleError(err, res);
+    }
+});
+
+// Route to fetch all questions associated with a specific tag ID
+router.get('/tag_id/:tag_id/questions', async (req, res) => {
+    try {
+        const questions = await Questions.find({ tags: req.params.tag_id }).exec();
+        res.send(questions);
+    } catch (err) {
+        handleError(err, res);
+    }
+});
+
 
 module.exports = router;
