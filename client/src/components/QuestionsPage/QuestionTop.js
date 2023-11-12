be7311c6f7ac53348c22ca3f8dd22f36031b227e
chrism097 <christian.montano@stonybrook.edu>
2023-11-12 04:03:27 -0500

    QuestionTop

--- client/src/components/QuestionsPage/QuestionTop.js
+++ client/src/components/QuestionsPage/QuestionTop.js
@@ -1,43 +1,65 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from "react";
 import "../../stylesheets/QuestionTop.css";
-import AskQuestionButton from './AskQuestionButtonForHomePage';
-import Filter from './Filter';
-import axios from 'axios'; 
+import AskQuestionButton from "./AskQuestionButtonForHomePage";
+import Filter from "./Filter";
+import axios from "axios";
+import Helper from "../../utils/Helper";
 
-const QuestionTop = ({ onAskQuestionPress, setFilter, searchTerm }) => {
+const QuestionTop = ({
+  onAskQuestionPress,
+  setFilter,
+  searchTerm,
+  selectedTag,
+}) => {
   const [questionCount, setQuestionCount] = useState(0);
+  const helper = new Helper();
+
+  const fetchQuestionCount = useCallback(
+    async (filter) => {
+      let endpoint = "http://localhost:8000/posts/questions";
+      if (selectedTag) {
+        endpoint = `http://localhost:8000/posts/tags/tag_id/${selectedTag}/questions`;
+      }
+      if (filter) {
+        endpoint += `/${filter}`;
+      }
+
+      try {
+        const response = await axios.get(endpoint);
+        let filteredQuestions = response.data;
+        if (searchTerm) {
+          filteredQuestions = await helper.filterQuestionsBySearchTerm(searchTerm, filteredQuestions);
+        }
+        setQuestionCount(filteredQuestions.length);
+      } catch (error) {
+        console.error("Error fetching questions count:", error);
+      }
+    },
+    [selectedTag, searchTerm]
+  );
 
   useEffect(() => {
-    // Fetch the count of all questions using axios
-    axios.get('http://localhost:8000/posts/questions')
-      .then(response => {
-        setQuestionCount(response.data.length);
-      })
-      .catch(error => {
-        console.error("Error fetching all questions count:", error);
-      });
+    fetchQuestionCount();
+  }, [selectedTag, searchTerm, fetchQuestionCount]);
 
-  }, []);
-
-  /*
-  const handleFilterChange = useCallback((newFilter) => {
-    setFilter(newFilter);
-    if (newFilter === 'unanswered') {
-      setQuestionCount(unansweredCount);
-    } else {
-      setQuestionCount(numOfQuestions);
-    }
-  }, [setFilter, unansweredCount, numOfQuestions]);
-  */
+  const handleFilterChange = useCallback(
+    (newFilter) => {
+      setFilter(newFilter);
+      fetchQuestionCount(newFilter);
+    },
+    [setFilter, fetchQuestionCount]
+  );
 
   return (
-    <div className='questionTop'>
-        <h1 id='allQuestionID'>  {searchTerm ? 'Search Results' : 'All Questions'}</h1>
-        <AskQuestionButton onPress={onAskQuestionPress}/>
-        <h4 id='numQuestion'>{`${questionCount} Questions`}</h4>
-        <Filter onSetFilter={() => {}} /> {/* Temporarily passing an empty function */}
+    <div className="questionTop">
+      <h1 id="allQuestionID">
+        {searchTerm ? "Search Results" : "All Questions"}
+      </h1>
+      <AskQuestionButton onPress={onAskQuestionPress} />
+      <h4 id="numQuestion">{`${questionCount} Questions`}</h4>
+      <Filter onSetFilter={handleFilterChange} />
     </div>
   );
-}
+};
 
 export default QuestionTop;
