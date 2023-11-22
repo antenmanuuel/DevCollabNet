c1f1c989f9d9288c0e9f3c5379fc22b4ac2e1ea3
Anten Manuuel <anten.manuuel@stonybrook.edu>
2023-11-22 11:51:59 -0500

    implement buttons for next and previous if there is more than 5 questions or answers

--- client/src/components/SelectedQuestionPage/AnswersTable.jsx
+++ client/src/components/SelectedQuestionPage/AnswersTable.jsx
@@ -9,10 +9,13 @@ import {
   Box,
   List,
   ListItem,
+  Button,
 } from "@mui/material";
 
 const AnswersTable = ({ questionId }) => {
   const [answers, setAnswers] = useState([]);
+  const [startIndex, setStartIndex] = useState(0);
+  const answersPerPage = 5; // Number of answers to display per page
   const helper = new Helper();
 
   useEffect(() => {
@@ -29,52 +32,80 @@ const AnswersTable = ({ questionId }) => {
       });
   }, [questionId]);
 
+  const handleNext = () => {
+    setStartIndex((prev) => {
+      const nextIndex = prev + answersPerPage;
+      return nextIndex < answers.length ? nextIndex : 0;
+    });
+  };
+
+  const handlePrev = () => {
+    setStartIndex((prev) => {
+      const prevIndex = prev - answersPerPage;
+      return prevIndex >= 0
+        ? prevIndex
+        : (Math.ceil(answers.length / answersPerPage) - 1) * answersPerPage;
+    });
+  };
+
+  const displayedAnswers = answers.slice(
+    startIndex,
+    startIndex + answersPerPage
+  );
+  const isPrevDisabled = startIndex === 0;
+  const isNextDisabled = startIndex + answersPerPage >= answers.length;
+
   return (
-    <Box
-      sx={{
-        width: "91.66%",
-        marginLeft: "210px",
-        marginY: 6,
-        overflow: "auto",
-      }}
-    >
-      <Table sx={{ width: "100%", borderCollapse: "collapse" }}>
-        <TableBody>
-          {answers.map((answer) => (
-            <TableRow
-              key={answer._id}
-              sx={{
-                "&:last-child td, &:last-child th": { border: 0 },
-                borderBottom: 4,
-                borderTop: 4,
-                borderColor: "grey.500",
-                borderStyle: "dotted",
-              }}
-            >
-              <TableCell
+    <Box sx={{ width: "91.66%", marginLeft: "210px", marginY: 6 }}>
+      <Box sx={{ overflow: "auto", height: "305px", mb: 2 }}>
+        <Table sx={{ width: "100%", borderCollapse: "collapse" }}>
+          <TableBody>
+            {displayedAnswers.map((answer) => (
+              <TableRow
+                key={answer._id}
                 sx={{
-                  fontWeight: "bold",
-                  width: "50%",
-                  overflow: "auto",
-                  padding: 4,
+                  "&:last-child td, &:last-child th": { border: 0 },
+                  borderBottom: 4,
+                  borderTop: 4,
+                  borderColor: "grey.500",
+                  borderStyle: "dotted",
                 }}
               >
-                {helper.renderTextWithLinks(answer.text)}
-              </TableCell>
-              <TableCell sx={{ paddingLeft: 60 }}>
-                <List sx={{ listStyle: "none", padding: 0 }}>
-                  <ListItem sx={{ color: "error.main", paddingBottom: 0 }}>
-                    {answer.ans_by}
-                  </ListItem>
-                  <ListItem sx={{ color: "text.secondary" }}>
-                    answered {helper.formatDate(new Date(answer.ans_date_time))}
-                  </ListItem>
-                </List>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+                <TableCell
+                  sx={{
+                    fontWeight: "bold",
+                    width: "50%",
+                    overflow: "auto",
+                    padding: 4,
+                  }}
+                >
+                  {helper.renderTextWithLinks(answer.text)}
+                </TableCell>
+                <TableCell sx={{ paddingLeft: 60 }}>
+                  <List sx={{ listStyle: "none", padding: 0 }}>
+                    <ListItem sx={{ color: "error.main", paddingBottom: 0 }}>
+                      {answer.ans_by}
+                    </ListItem>
+                    <ListItem sx={{ color: "text.secondary" }}>
+                      answered{" "}
+                      {helper.formatDate(new Date(answer.ans_date_time))}
+                    </ListItem>
+                  </List>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Box>
+
+      <Box sx={{ display: "flex", justifyContent: "center", mt: 2 }}>
+        <Button onClick={handlePrev} disabled={isPrevDisabled}>
+          Prev
+        </Button>
+        <Button onClick={handleNext} disabled={isNextDisabled}>
+          Next
+        </Button>
+      </Box>
     </Box>
   );
 };
