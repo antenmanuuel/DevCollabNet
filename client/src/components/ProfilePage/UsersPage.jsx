25ecce204ca77f6223f4fe38b6328ee376f266a3
chrism097 <christian.montano@stonybrook.edu>
2023-12-05 03:42:05 -0500

    UserPage and ProfilePage updated

--- client/src/components/ProfilePage/UsersPage.jsx
+++ client/src/components/ProfilePage/UsersPage.jsx
@@ -10,219 +10,235 @@ import {
   Button,
 } from "@mui/material";
 
+const UsersPage = () => {
 
-const UsersPage = ({sessionData}) => {
+  const [sessionData, setSessionData] = useState({
+    loggedIn: false,
+    username: "",
+    email: "",
+    created_at: "",
+    reputation: 0,
+  });
 
+  const [userQuestions, setUserQuestions] = useState([]);
 
-
-    return (
-      <Box sx={{ width: "85.3%", paddingBottom: 5, position: "absolute",border: 3,
-      borderColor: "black",
-      borderStyle: "dotted",
-      left: "275.5px",
-      height: "900px",
-      borderLeft: 0,
-      borderRight: 0,
-      borderTop: 0,
-      borderbottom: 0
-      //make border top,right,bottom to 0 when done
-       }}>
-
-      <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            width: "100%",
-            left: "275.5px",
-            border: 3,
-            borderTop: 0 ,
-            borderRight: 0,
-            borderLeft: 0 ,
-            borderColor: "blue",
-            borderStyle: "dotted",
-            height: "175px"
-          }}
-        >
-        
-      <Typography
-        variant="h4"
-        sx={{
-          position: "absolute",
-          top: "10px",
-          left: "30px",
-          fontSize: "25px",
-          fontWeight: "bolder",
-          border: 3,
-          borderColor: "purple",
-          borderStyle: "dotted",
-        }}
-      >
-        User Profile: SessionUSERNAME 
-      </Typography>
-
-      <Typography
-        variant="h6"
-        sx={{
-          position: "absolute",
-          top: "50px",
-          left: "30px",
-          fontSize: "18px",
-          border: 3,
-          borderColor: "red",
-          borderStyle: "dotted",
-        }}
-      >
-        Member for : TimeHERE
-      </Typography>
-
-      <Typography
-        variant="h2"
-        sx={{
-          position: "absolute",
-          top: "90px",
-          left: "30px",
-          fontSize: "15px",
-          border: 3,
-          borderColor: "red",
-          borderStyle: "dotted",
-        }}
-      >
-        Reptuation Score: ScoreHere
-      </Typography>
-
-      <Typography
-        variant="h1"
-        sx={{
-          position: "absolute",
-          top: "130px",
-          left: "550px",
-          fontSize: "18px",
-          fontWeight: "bolder",
-          border: 3,
-          borderColor: "purple",
-          borderStyle: "dotted",
-        }}
-      >
-        All "Tags/Questions" created by "User"
-      </Typography>
-      </Box>
-      <Table  sx={{ width: "100%" }}>
-        <TableRow sx={{
-                    borderBottom: 4,
-                    borderTop: 4,
-                    borderColor: "orange",
-                    borderStyle: "dotted",
-                  }}> 
-          
-          <TableCell
-          sx={{
-                    borderBottom: 4, //change this to 3 or 2 when done
-                    borderRight: 4, //delete bottomright this once done 
-                    borderColor: "yellow",
-                    borderStyle: "dotted",
-                    width: "65%"
-                  }}>
-          <Typography
-                      /*onClick={() =>
-                        handleQuestionTitleClick()
-                      }*/
-                      sx={{
-                        cursor: "pointer",
-                        color: "black",
-                        fontSize: "large",
-                      }}
-                    >
-                      LINK
-                    </Typography>
-
-          </TableCell>
-
-          <TableCell
-          sx={{
-                    borderBottom: 4, //change this to 3 or 2 when done
-                    borderRight: 4, //delete bottomright this once done 
-                    borderColor: "green",
-                    borderStyle: "dotted",
-                    width: "17.5%"
-                  }}>
-          <Typography
-                      /*onClick={() =>
-                        handleDeleteClick()
-                      }*/
-                      sx={{
-                        color: "red",
-                        fontSize: "medium",
-                      }}
-                    >
-                      Time Added
-                    </Typography>
-          </TableCell>
-
-          <TableCell
-          sx={{
-                    borderBottom: 4, //change this to 3 or 2 when done
-                    borderRight: 4, //delete bottomright this once done 
-                    borderColor: "red",
-                    borderStyle: "dotted",
-                    width: "17.5%",
-                    paddingLeft: "70px"
-          
-                  }}>
-          <Typography
-                      /*onClick={() =>
-                        handleDeleteClick()
-                      }*/
-                      sx={{
-                        cursor: "pointer",
-                        color: "common.white",
-                        fontSize: "medium",
-                        backgroundColor: "red",
-                        border: 3,
-                        borderRadius: '16px', 
-                        textAlign: "center",
-                        borderColor: "red",
-                        width: "100px",
-                      
-                      }}
-                    >
-                      Delete
-                    </Typography>
-          </TableCell>
-        
-        
-        </TableRow>
-      
-      
-      
-      
-      
-      
-       </Table>
-       <Box sx={{ width: "100%", paddingBottom: 5, position: "absolute",border: 3,
-      borderColor: "black",
-      borderStyle: "dotted",
-      bottom: "0px",
-      top: "775px",
-      borderBottom:0,
-      borderRight: 0,
-      borderLeft: 0,
-      
-
-       }}>  
-        <Button variant="contained" sx={{ marginTop:"5px", marginRight:"5px", left:"450px" }}>All Tags Made by "User"</Button>
-        <Button variant="contained" sx={{ marginTop:"5px", left:"450px" }}> Questions By "User" that have Answers</Button>
-
-       </Box>
-    </Box>
-
-
-
-
-
-
-      );
+  useEffect(() => {
+    const fetchSessionData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8000/users/session");
+        if (response.data && response.data.loggedIn) {
+          setSessionData({
+            loggedIn: true,
+            username: response.data.username,
+            email: response.data.email,
+            created_at: response.data.created_at,
+            reputation: response.data.reputation,
+          });
+        }
+      } catch (error) {
+        console.error("Error fetching session data:", error);
+      }
     };
 
+    fetchSessionData();
+  }, []);
 
-    
+  useEffect(() => {
+    const fetchUserQuestions = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8000/posts/questions/byUsername/${sessionData.username}`
+        );
+        setUserQuestions(response.data);
+      } catch (error) {
+        console.error("Error fetching user's questions:", error);
+      }
+    };
+
+    if (sessionData.username) {
+      fetchUserQuestions();
+    }
+  }, [sessionData.username]);
+
+  const formatDate = (postTime) => {
+    const now = new Date();
+    const diffInSeconds = (now - new Date(postTime)) / 1000;
+
+    if (diffInSeconds < 60) {
+      return `${Math.round(diffInSeconds)} seconds ago`;
+    } else if (diffInSeconds < 3600) {
+      return `${Math.round(diffInSeconds / 60)} minutes ago`;
+    } else if (diffInSeconds < 86400) {
+      return `${Math.round(diffInSeconds / 3600)} hours ago`;
+    } else {
+      return new Date(postTime).toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      });
+    }
+  };
+
+  const memberSince = sessionData.created_at
+    ? formatDate(new Date(sessionData.created_at))
+    : "Loading...";
+
+  const handleDeleteQuestion = async (questionId) => {
+    try {
+      await axios.delete(`http://localhost:8000/posts/questions/${questionId}`);
+      setUserQuestions(
+        userQuestions.filter((question) => question._id !== questionId)
+      );
+    } catch (error) {
+      console.error("Error deleting question:", error);
+    }
+  };
+
+  return (
+    <Box
+      sx={{
+        width: "85.3%",
+        paddingBottom: 5,
+        position: "absolute",
+        border: 3,
+        borderColor: "black",
+        borderStyle: "dotted",
+        left: "275.5px",
+        height: "900px",
+        borderLeft: 0,
+        borderRight: 0,
+        borderTop: 0,
+        borderBottom: 0,
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          width: "100%",
+          left: "275.5px",
+          border: 3,
+          borderTop: 0,
+          borderRight: 0,
+          borderLeft: 0,
+          borderStyle: "dotted",
+          height: "175px",
+        }}
+      >
+        <Typography
+          variant="h4"
+          sx={{
+            position: "absolute",
+            top: "10px",
+            left: "30px",
+            fontSize: "25px",
+            fontWeight: "bolder",
+          }}
+        >
+          User Profile:{" "}
+          {sessionData.loggedIn ? sessionData.username : "Loading..."}
+        </Typography>
+        <Typography
+          variant="h6"
+          sx={{
+            position: "absolute",
+            top: "50px",
+            left: "30px",
+            fontSize: "18px",
+          }}
+        >
+          Member for: {memberSince}
+        </Typography>
+        <Typography
+          variant="h2"
+          sx={{
+            position: "absolute",
+            top: "90px",
+            left: "30px",
+            fontSize: "15px",            
+          }}
+        >
+          Reputation Score:{" "}
+          {sessionData.loggedIn ? sessionData.reputation : "Loading..."}
+        </Typography>
+        <Typography
+          variant="h1"
+          sx={{
+            position: "absolute",
+            top: "130px",
+            left: "550px",
+            fontSize: "18px",
+            fontWeight: "bolder",
+            
+          }}
+        >
+          All "Tags/Questions" created by {sessionData.username}
+        </Typography>
+      </Box>
+      <Table sx={{ width: "100%" }}>
+        <TableBody>
+          {userQuestions.map((question) => (
+            <TableRow key={question._id} sx={{borderBottom:"3px", borderStyle:"dotted"}}>
+              <TableCell sx={{ width: "65%" }}>
+                <Typography
+                  sx={{ cursor: "pointer", color: "black", fontSize: "large" }}
+                >
+                  {question.title}
+                </Typography>
+              </TableCell>
+              <TableCell sx={{ width: "17.5%" }}>
+                <Typography sx={{ color: "gray", fontSize: "medium" }}>
+                  asked {formatDate(question.ask_date_time)}
+                </Typography>
+              </TableCell>
+              <TableCell sx={{ width: "17.5%", paddingLeft: "70px" }}>
+                <Button
+                  onClick={() => handleDeleteQuestion(question._id)}
+                  sx={{
+                    color: "common.white",
+                    fontSize: "medium",
+                    backgroundColor: "red",
+                    border: 3,
+                    borderRadius: "16px",
+                    textAlign: "center",
+                    borderColor: "red",
+                    width: "100px",
+                  }}
+                >
+                  Delete
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <Box
+        sx={{
+          width: "100%",
+          paddingBottom: 5,
+          position: "absolute",
+          border: 3,
+          borderColor: "black",
+          borderStyle: "dotted",
+          bottom: "0px",
+          top: "775px",
+          borderBottom: 0,
+          borderRight: 0,
+          borderLeft: 0,
+        }}
+      >
+        <Button
+          variant="contained"
+          sx={{ marginTop: "5px", marginRight: "5px", left: "450px" }}
+        >
+          All Tags Made by "User"
+        </Button>
+        <Button variant="contained" sx={{ marginTop: "5px", left: "450px" }}>
+          Questions By "User" that have Answers
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
 export default UsersPage;
