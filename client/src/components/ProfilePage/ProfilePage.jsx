1d380c68bead7dff9efc82d707883e4fd695512d
chrism097 <christian.montano@stonybrook.edu>
2023-12-06 00:52:54 -0500

    UserPage with Tags,Answers,Questions new format

--- client/src/components/ProfilePage/ProfilePage.jsx
+++ client/src/components/ProfilePage/ProfilePage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import UsersPage from "./UsersPage";
 import AdminsPage from "./AdminsPage";
 import { Typography } from "@mui/material";
+import UsersPage from "./UsersPage";
 
 const ProfilePage = ({ sessionData }) => {
     // Check if the user is not logged in
+   
     if (!sessionData || !sessionData.username) {
         return <Typography sx={{paddingLeft: "300px", paddingTop:"40px", color:"red", fontSize:"20px"}}>Error: You must be logged in to view this page.</Typography>;
     }
