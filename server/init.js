b3f6e7678545da892fb84c4b6464c855c66f97e8
Anten Manuuel <anten.manuuel@stonybrook.edu>
2023-12-30 21:48:59 -0500

    fix warning when running the project

--- server/init.js
+++ server/init.js
@@ -7,7 +7,7 @@ const Answer = require("./models/answers");
 const Comment = require("./models/comments");
 
 let mongoDB = "mongodb://127.0.0.1:27017/fake_so";
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mongoDB);
 
 let db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
